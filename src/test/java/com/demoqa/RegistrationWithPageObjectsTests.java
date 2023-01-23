2d1d67af274c568c636b8b7137ea93daed1c4457
alekseibulatov <59173362+alekseibulatov@users.noreply.github.com>
2023-01-23 17:02:59 +0300

    first commit

---
+++ src/test/java/com/demoqa/RegistrationWithPageObjectsTests.java
@@ -0,0 +1,64 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+import pages.RegistrationPage;
+
+
+import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class RegistrationWithPageObjectsTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void successRegistration() {
+        String userName = "Aleksei";
+        String lastName = "Ivanov";
+        String genderWrapper = "Male";
+        String userEmail = "sdd@qaguru.ru";
+
+        String userPhoneNumber = "79008763421";
+
+        new RegistrationPage().openPage();
+        new RegistrationPage().setFirstName(userName);
+        new RegistrationPage().setLastName(lastName);
+        new RegistrationPage().setUserEmail(userEmail);
+        new RegistrationPage().setGender(genderWrapper);
+
+
+
+
+
+        $("#userNumber").setValue("79008763421");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker").shouldBe(Condition.visible);
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("1981");
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("March");
+        $(".react-datepicker__day--003:not(.react-datepicker__day--outside-month)").click();
+        $("#subjectsInput").setValue("Math").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFromClasspath("original.jpg");
+        $("#currentAddress").setValue("SPb. Street ");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+        $(".modal-content").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Aleksei Ivanov"), text("sdd@qaguru.ru"),
+                text("Male"), text("7900876342"), text("03 March,1981"), text("Maths"),
+                text("Sports"), text("original.jpg"), text("SPb. Street"), text("NCR Delhi"));
+    }
+}
---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,41 @@
+package pages;
+
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
+
+public class RegistrationPage {
+    private final String TITLE_TEXT = "Practice Form";
+
+    private SelenideElement
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            userGender = $("#genterWrapper");
+
+    public void openPage() {
+        open("/automation-practice-form");
+        $(".main-header").shouldHave(text(TITLE_TEXT));
+        executeJavaScript("$('#fixedban').remove()");
+        executeJavaScript("$('footer').remove()");
+    }
+
+    public void setFirstName(String value) {
+        firstNameInput.setValue(value);
+    }
+
+    public void setLastName(String value) {
+        lastNameInput.setValue(value);
+    }
+
+    public void setUserEmail(String value) {
+        userEmailInput.setValue(value);
+    }
+
+    public void setGender(String value) {
+        userGender.$(byText(value)).click();
+    }
+}
